9a102c677c1d7fea8bb604a492e8b82a2f1d6997
Raddos <raddos@gmail.com>
2026-03-25 12:30:51 +0100

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+//Branched for old version and additonal C style like data structures and algorithms 
+int main(int argc, char *argv[])
+{
+	std::cout << "Hello!" << std::endl;
+	return 0;
+}
